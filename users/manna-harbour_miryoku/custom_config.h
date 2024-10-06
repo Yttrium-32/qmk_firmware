a67b1b780250f46e23d5cc74599f73220303aeff
Shresth Prasad <shresthprasad7@gmail.com>
2024-10-06 14:51:54 +0530

    modify tap layer to replace del with return to base

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,3 +5,9 @@
 
 #pragma once
 
+// Replace del key with return to base macro on tap layer
+#define MIRYOKU_LAYER_TAP \
+KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
+KC_A,              KC_S,              KC_D,              KC_F,              KC_G,              KC_H,              KC_J,              KC_K,              KC_L,              KC_QUOT,           \
+KC_Z,              KC_X,              KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,            KC_SLSH,           \
+U_NP,              U_NP,              KC_ESC,            KC_SPC,            KC_TAB,            KC_ENT,            KC_BSPC,           TD(U_TD_U_BASE),   U_NP,              U_NP
